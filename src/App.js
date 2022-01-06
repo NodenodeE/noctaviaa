8f983d34a77f2d3d3cd140710784e6ef770fbc49
NodenodeE <erika.norstrom@stud.sundsgarden.se>
2022-01-06 14:27:08 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import poolImg from "./assets/images/pool.png";
+import sheetImg from "./assets/images/sheet.png";
+import orangesImg from "./assets/images/oranges.png";
+import Modal from "react-bootstrap/Modal";
+
+console.log(poolImg);
+console.log(sheetImg);
+console.log(orangesImg);
+
+function dropFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
 function App() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav className="dropdown">
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header>
+              <p>Not available, very busy and famous.</p>
+              <a onClick={handleClose}>Close</a>
+            </Modal.Header>
+          </Modal>
+          <a onClick={dropFunction} className="dropbtn">
+            More
+          </a>
+          <div id="myDropdown" className="dropdown-content">
+            <a>Work</a>
+            <a>About</a>
+            <a onClick={handleShow}>Hire me</a>
+          </div>
+        </nav>
       </header>
-    </div>
+      <main>
+        <section className="headerTitle">
+          <h1>Octavia Design</h1>
+        </section>
+        <div className="mainImages">
+          <img
+            src={poolImg}
+            className="img-pool"
+            alt="abstract photograph of someone diving udner the water in a pool"
+          />
+          <img
+            src={sheetImg}
+            className="img-sheet"
+            alt="naked girl reaching for the sky wrapped in white sheet"
+          />
+          <img
+            src={orangesImg}
+            className="img-citrus"
+            alt="karaff pouring water into glass in the sun besides slices of bloodgrape"
+          />
+        </div>
+      </main>
+    </>
   );
 }
 
